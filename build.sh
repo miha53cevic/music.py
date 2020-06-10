4a7289efaedff8aae010d7689cbc81876d4a62ef
miha53cevic <44951515+miha53cevic@users.noreply.github.com>
2020-06-10 15:20:54 +0200

    add sudo if exists

--- build.sh
+++ build.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 # python libraries
+sudo pip3 install pygame
+sudo pip3 install mutagen
+sudo pip3 install pyinstaller
+
+# non sudo if sudo doesn't exist
 pip3 install pygame
 pip3 install mutagen
 pip3 install pyinstaller
